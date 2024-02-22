b7da61dc7dc2c0c67436de4bc67451bc733fa5ae
ad3rinto <aad3rinto@gmail.com>
2024-02-22 15:56:18 +0000

    cxcv

--- main.py
+++ main.py
@@ -1,2 +1,25 @@
 import os
+import ssl
+
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    # Legacy Python that doesn't verify HTTPS certificates by default
+    pass
+else:
+    # Handle target environment that doesn't support HTTPS verification
+    ssl._create_default_https_context = _create_unverified_https_context
 from twilio.rest import Client
+
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
+client = Client(account_sid, auth_token)
+
+message = client.messages \
+                .create(
+                     body="Join Earth's mightiest heroes. Like Kevin Bacon.",
+                     from_='+447846226806',
+                     to='+447488879613'
+                 )
+
+print(message.sid)
